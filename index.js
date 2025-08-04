188ef010ebd67d8cf794a97590b5750d0d549b78
Abhinayydv <yadavabhi2654@gmail.com>
2025-08-04 05:57:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('new-user', (username) => {
+    socket.username = username;
+    socket.broadcast.emit('chat message', `${username} joined the chat`);
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', `${socket.username}: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.username) {
+      io.emit('chat message', `${socket.username} left the chat`);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
